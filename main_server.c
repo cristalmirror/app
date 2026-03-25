bc5f7b70c4b2fda753e459593c6d5885ee14f5ec
cristalmirror <crisrojari@gmail.com>
2026-03-25 00:23:28 -0300

    app modify server and app

--- main_server.c
+++ main_server.c
@@ -9,7 +9,7 @@
 int main(int argc,char *argv[]) {
     int cont = 0; 
 reboot:
-    if (cont > 0 ) printf("[SERVIDOR]: Se reinicion el servidor para una nueva conexionc\n  Numero de Conexiones: [%i]\n",cont);
+    if (cont > 0 ) printf("[SERVIDOR]: Se reinicion el servidor para una nueva conexion...\n  Numero de Conexiones: [%i]\n",cont);
 
     int server_fd, new_socket, opt = 1;
     struct sockaddr_in address;
@@ -20,7 +20,7 @@ reboot:
         perror("Socket fallido");
         exit(EXIT_FAILURE);
     }
-
+    //clean the port if the connection need last reboot
     if (setsockopt(server_fd,SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("Error en setsocketopt");
         exit(EXIT_FAILURE);
@@ -30,25 +30,26 @@ reboot:
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(1234);
-
+    //assignations of IP and port 
     if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("Error en bind");
         exit(EXIT_FAILURE);
     }
-
+    //listen port
     if (listen(server_fd, 3) < 0) {
         perror("Error en listen");
         exit(EXIT_FAILURE);
     }
 
     printf("[SERVIDOR]: Esperando un conexion en el puerto 1234...\n");
-
+    //handshake 
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
         perror("Error en accept");
         exit(EXIT_FAILURE);
     }
 
     int bytes_leidos;
+    //reception of messages 
     while ((bytes_leidos = recv(new_socket, buffer,1024, 0)) > 0) {
         printf("[CLIENTE]: %s\n", buffer);
         memset(buffer, 0, 1024);
@@ -56,7 +57,8 @@ reboot:
 
     // el servidor imprime
     printf("[SERVIDOR]:Cliente se desconector\n");
-
+    
+    //close sockets ando connection
     close(new_socket);
     close(server_fd);
     cont++;
